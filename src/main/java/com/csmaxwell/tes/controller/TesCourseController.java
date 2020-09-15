7324a36da308e080fe060e509264ff12aecfdb5e
maxwell <zhong.maxwell@gmail.com>
2020-09-15 15:16:58 +0800

    更新sql

--- src/main/java/com/csmaxwell/tes/controller/TesCourseController.java
+++ src/main/java/com/csmaxwell/tes/controller/TesCourseController.java
@@ -13,6 +13,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
@@ -20,7 +21,7 @@ import java.util.List;
  * S
  * Created by maxwell on 2020/9/15.
  */
-@Controller
+@RestController
 @Api(tags = "TesCourseController", description = "课程管理")
 @RequestMapping("/course")
 public class TesCourseController {
