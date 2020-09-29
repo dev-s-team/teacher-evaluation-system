369533282bf02cd4d5a3a24c3ef0ac6cf5b97d93
ly <2316496225@qq.com>
2020-09-29 11:01:19 +0800

    更新角色管理

--- src/main/java/com/csmaxwell/tes/dao/TesPermissionMapper.java
+++ src/main/java/com/csmaxwell/tes/dao/TesPermissionMapper.java
@@ -1,7 +1,12 @@
 package com.csmaxwell.tes.dao;
 
 import com.csmaxwell.tes.domain.TesPermission;
+import org.apache.ibatis.annotations.Select;
 import tk.mybatis.mapper.common.Mapper;
 
+import java.util.List;
+
 public interface TesPermissionMapper extends Mapper<TesPermission> {
+    @Select("select * from tes_permission tp")
+    List<TesPermission> getPermissionList(Long permissionId);
 }
